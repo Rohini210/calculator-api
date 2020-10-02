7616d2d224f9e06ce05196628046ccebd84d3055
Rohini210 <prohini210@gmail.com>
2020-10-03 00:28:20 +0530

    index.js modified

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.post("/add", (req, res) => {
   let value1 = req.body.value1
   let value2 = req.body.value2
   if (typeof value1 === "string" || typeof value2 === "string") {
-    return res.status(400).json({ message: "Invalid Data Types" })
+    return res.status(400).json({ message: "invalid data types" })
   }
 
   if (value1 > 1000000 || value2 > 1000000) {
@@ -33,7 +33,7 @@ app.post("/add", (req, res) => {
   }
 
   let sum = value1 + value2
-  res.status(200).json({ message: "the sum of given two numbers", sum })
+  res.status(200).json({ message: "the sum of given two number", sum: sum })
 })
 
 //Subtraction:
@@ -42,15 +42,15 @@ app.post("/subtract", (req, res) => {
   let value1 = req.body.value1
   let value2 = req.body.value2
   if (typeof value1 === "string" || typeof value2 === "string") {
-    return res.status(400).json({ message: "Invalid Data Types" })
+    return res.status(400).json({ message: "invalid data types" })
   }
 
-  if (value1 < 1000000 || value2 < 1000000) {
+  if (value1 < -1000000 || value2 < -1000000) {
     return res.status(400).json({ message: "Underflow" })
   }
 
   let diff = value1 - value2
-  res.status(200).json({ message: "the difference of given two numbers", diff })
+  res.status(200).json({ message: "the difference of given two number", difference: diff })
 })
 
 //Multiplication:
@@ -59,15 +59,15 @@ app.post("/multiply", (req, res) => {
   let value1 = req.body.value1
   let value2 = req.body.value2
   if (typeof value1 === "string" || typeof value2 === "string") {
-    return res.status(400).json({ message: "Invalid Data Types" })
+    return res.status(400).json({ message: "invalid data types" })
   }
 
   if (value1 > 1000000 || value2 > 1000000) {
-    return res.status(400).json({ message: "Underflow" })
+    return res.status(400).json({ message: "Overflow" })
   }
 
   let mul = value1 * value2
-  res.status(200).json({ message: "the product of given two numbers", mul })
+  res.status(200).json({ message: "The product of given numbers", product: mul })
 })
 
 //Division:
@@ -76,7 +76,7 @@ app.post("/division", (req, res) => {
   let value1 = req.body.value1
   let value2 = req.body.value2
   if (value2 === 0) {
-    return res.status(400).json({ message: "Cannot be divided by Zero" })
+    return res.status(400).json({ message: "Cannot divide by zero" })
   }
 
   let div = value1 / value2
